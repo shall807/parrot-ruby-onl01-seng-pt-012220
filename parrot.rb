df2ef64ff13786aff1a2fa907b5b24159111cbe6
Stevi Hall <career.sh.work@gmail.com>
2020-02-03 19:44:21 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,13 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot
+
+ puts "Squawk!"
+ return "Squawk!"
+#end
+
+
+ parrot("Pretty Bird")
+ puts ("parrot")
+ end
